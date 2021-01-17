b2e1975b47dc91ca1afd209b703e4ec2c03cf436
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-01-17 12:27:29 -0700

    variables added to js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,42 @@
-// Assignment Code
+//variables
+
+var lowerCasedLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasedLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericalCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacters = [':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.', '@', '%', '!', '#', '$', '^', '?', '+', '\\', '/', "'"];
+
+//Prompting user for preffered password length
+
+
+//storing the users length preference
+
+//checking if user entered a valid number and length
+
+//User chooses special characters
+
+
+//user chooses including numbers in the password
+
+//user chooses including lowercase letters in the password
+
+
+//user chooses including uppercase letters in the password
+
+//Store the users anwers 
+
+
+//Generating password
+
+
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
