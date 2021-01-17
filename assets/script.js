286f0caf1f0a06a369c52a34013a2293e3b19f61
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-01-17 13:16:53 -0700

    confirm messages added

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,45 @@ var numericalCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialCharacters = [':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.', '@', '%', '!', '#', '$', '^', '?', '+', '\\', '/', "'"];
 
 //Prompting user for preffered password length
+function getPassInput() {
+    //storing the users length preference
+    var length = parseInt(
+        prompt("How long would you like your password. Please pick between 8 and 129")
+    );
+    //checking if user entered a valid number and length, if not valid, send an alert    
+    if (isNaN(length) ===true) {
+        alert("Please provide a number");
+        return;
+    }
+
+    if (length > 129) {
+        alert("Please provide a number less than 129");
+        return;
+    }
+
+    if (length < 8) {
+        alert("Please provide a number larger than 7");
+    }
+     //user chooses if they want to include lowercase letters, uppercase letters, numbers & special characters in the password using confirm
+    var hasLowerCasedLetters = confirm(
+        "Click OK to include lowercase letters in password."
+    );
+
+    var hasUpperCasedLetters = confirm(
+        "Click OK to include uppercase letters in password."
+    );
+
+    var hasNumericalCharacters = confirm(
+        "Click OK to include numbers in password."
+    );
+
+    var hasSpecialCharacters = confirm(
+        "Click OK to include special characters in password."
+    )
+
+}
 
 
-//storing the users length preference
-
-//checking if user entered a valid number and length
-
-//User chooses special characters
-
-
-//user chooses including numbers in the password
-
-//user chooses including lowercase letters in the password
-
-
-//user chooses including uppercase letters in the password
 
 //Store the users anwers 
 
